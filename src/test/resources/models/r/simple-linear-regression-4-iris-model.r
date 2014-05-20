cf78f3bb44908c2abbe1800e1f89f87d2b09a173
Thomas Darimont <tdarimont@gopivotal.com>
2014-05-21 01:08:38 +0200

    Added abstraction to interact with r-project.
    Added basic abstractions to interface with r-project.

---
+++ src/test/resources/models/r/simple-linear-regression-4-iris-model.r
@@ -0,0 +1,48 @@
+library(pmml)
+
+#Splitts the given dataset in a training dataset(trainset) and test dataset(testset)
+splitDataFrame <- function(dataframe, seed = NULL, n = trainSize) {
+  
+  if (!is.null(seed)){
+    set.seed(seed)
+  }
+  
+  index <- 1:nrow(dataframe)
+  trainindex <- sample(index, n)
+  trainset <- dataframe[trainindex, ]
+  testset <- dataframe[-trainindex, ]
+  
+  list(trainset = trainset, testset = testset)
+}
+
+loadData <- function(){
+  #load the iris data
+  data(iris)
+  
+  #Split the iris dataset into test and train sets
+  dataSets <- splitDataFrame(iris, seed = 1337, n= round(0.95 * nrow(iris)))
+}
+
+buildModel <- function(dataSets){
+  model <- lm(Petal.Width ~ Petal.Length, data=dataSets$trainset)
+}
+
+exportPmml <- function(model, modelName=NULL, externalId=NULL){
+  pmml.lm(model,model.name=paste(modelName,externalId,sep = ";"))
+}
+
+# called from outside
+rebuildModel <- function(modelName=NULL, externalId=NULL){
+  dataSet = loadData()
+  model = buildModel(dataSet)
+  pmmlSource = exportPmml(model, modelName, externalId)
+  cat(toString(pmmlSource))
+}
+
+# called from outside
+benchModel <- function(id){
+  
+  dataSets = loadData()
+  model = buildModel(dataSets)
+  summary(model)
+}
